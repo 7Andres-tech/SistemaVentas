fe8372ed6148560ada2e1c1c478819b8fd8458ae
7Andres-tech <juancolosal0@gmail.com>
2024-12-13 15:48:48 -0500

    Update BD_Ventas.sql

--- BD_Ventas.sql
+++ BD_Ventas.sql
@@ -1,43 +1,70 @@
-use bd_ventas
+-- crear nuestra base de datos
+create database sistema_ventasbd;
+-- usamos la base de datos
+use sistema_ventasbd;
 
-select * from tb_Mozo;
-ALTER TABLE tb_Mozo MODIFY COLUMN idMozo INT NOT NULL AUTO_INCREMENT;
-DESCRIBE tb_Mozo;
-DESCRIBE tb_Cliente;
-select*from tb_cliente;
+-- crear tabla usuarios(Mozos)
+create table tb_Mozo(
+idMozo int (11) auto_increment primary key,
+nombre varchar(30) not null,
+password varchar(15) not null,
+telefono varchar(15) not null,
+correo varchar(45) not null,
+dirección text not null,
+DNI int(8) not null
+);
 
-select*from tb_producto;
 
-select*from tb_categoria;
 
-select p.idProducto, p.nombre, p.cantidad, p.precio, p.descripcion, 
-       c.descripcion as categoria, p.estado
-from tb_producto as p, tb_categoria as c
-where p.idCategoria = c.idCategoria;
+-- crear tabla cliente
+create table tb_Cliente(
+idCliente int (11) auto_increment primary key,
+nombre varchar(30) not null,
+telefono varchar(15) not null,
+direccion varchar(100) not null,
+DNI int(8) not null,
+correo varchar(45) not null
+);
 
-select*from tb_Ventas;
+-- crear tabla categoria
+create table tb_Categoria(
+idCategoria int (11) auto_increment primary key,
+descripcion varchar(200) not null,
+estado int(1) not null
+);
 
-select*from tb_detalleventa;
 
-SELECT fechaVenta, COUNT(fechaVenta) AS Ventas 
-FROM tb_Ventas 
-WHERE fechaVenta BETWEEN '2024-01-01' AND '2024-12-31' 
-GROUP BY fechaVenta;
 
-DESCRIBE tb_Mozo;
 
-ALTER TABLE tb_Mozo MODIFY COLUMN idMozo INT NOT NULL AUTO_INCREMENT;
+-- crear tabla producto
+create table tb_Producto(
+idProducto int (11) auto_increment primary key,
+nombre varchar(100) not null,
+cantidad int(11) not null,
+precio double(10,2) not null,
+descripcion varchar(200) not null,
+idCategoria int(11) not null,
+estado int(1) not null
+);
 
-ALTER TABLE tb_Mozo MODIFY COLUMN idMozo INT AUTO_INCREMENT;
-ALTER TABLE tb_Cliente MODIFY COLUMN idCliente INT AUTO_INCREMENT;
+-- crear tabla de venta
+create table tb_Venta(
+idVenta int (11) auto_increment primary key,
+idCliente int(11) not null,
+valorPagar double(10,2) not null,
+fechaVenta date not null,
+estado int(1) not null
+);
 
-INSERT INTO tb_Mozo (nombre, direccion, correo, password, telefono, DNI)
-VALUES ('Prueba Mozo', 'Dirección X', 'prueba@correo.com', '12345', '123456789', 98765432);
-
-ALTER TABLE tb_Mozo ADD COLUMN idMozo INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
-DESCRIBE tb_Mozo;
-
-ALTER TABLE tb_Mozo 
-    ADD COLUMN idMozo INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST;
-    
-    ALTER TABLE tb_Mozo DROP COLUMN idMozo;
+-- crear tabla detalle de venta
+create table tb_detalle_venta(
+idDetalleVenta int (11) auto_increment primary key,
+idVenta int (11) not null,
+idProducto int (11) not null,
+cantidad int(11) not null,
+precioUnitario double(10,2) not null,
+subtotal double(10,2) not null,
+descuento double(10,2) not null,
+totalPagar double(10,2) not null,
+estado int(1) not null
+);
